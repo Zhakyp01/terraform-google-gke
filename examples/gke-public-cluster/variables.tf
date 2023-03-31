13499669efc0161292b60effd0f3676f0fe9f011
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2023-03-31 15:07:58 +0600

    Update variables.tf

--- examples/gke-public-cluster/variables.tf
+++ examples/gke-public-cluster/variables.tf
@@ -6,16 +6,19 @@
 variable "project" {
   description = "The project ID where all resources will be launched."
   type        = string
+  default     = "student-project-379814"
 }
 
 variable "location" {
   description = "The location (region or zone) of the GKE cluster."
   type        = string
+  default     = "us-west1"
 }
 
 variable "region" {
   description = "The region for the network. If the cluster is regional, this must be the same region. Otherwise, it should be the region of the zone."
   type        = string
+  default     = "us-west1-a"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
